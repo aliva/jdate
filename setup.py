d40afdc3e97cdd1e28c44161e1205b670c169ef2
Ali Vakilzade <ali@vakilzade.ir>
2014-11-26 21:16:14 +0330

    try to create a setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="jdate",
+    version="0.0.1",
+    description="simple jalali date command (works like date)",
+    author="Ali Vakilzade",
+    author_email="ali@vakilzade.ir",
+    url="https://github.com/aliva/jdate",
+    scripts=["jdate",],
+    license="BSD 3-Clause",
+)
